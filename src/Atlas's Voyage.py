ca84880a80ebb1089d6e36e6c17562e36d60e5ae
Frostbyte-Cat1 <AndrewRimington01@gmail.com>
2021-07-04 18:16:32 +0800

    Update Atlas's Voyage.py

--- src/Atlas's Voyage.py
+++ src/Atlas's Voyage.py
@@ -6,39 +6,47 @@ py.init()
 #py.display.set_icon(programIcon)
 py.display.set_caption("Atlas's Voyage")
 screen = py.display.set_mode((720,480))
-screen.fill((255,255,255))
 
-color=(255,0,0)
-x = 100
-y = 445
-width = 30
-height = 30
-vel = 0.4
+class spaceship:
+    vel = 0.4
+    def __init__(self):
+        self.color = (255,0,0)
+        self.x = 100
+        self.y = 445
+        self.width = 30
+        self.height = 30
+        #Images of spaceship goes here
+        self.player = py.draw.rect(screen,self.color,(self.x,self.y,self.width,self.height))
+        pass
 
-running = True
-while running:
-    for event in py.event.get():
-        if event.type == py.QUIT:
-            running = False
+    
+    def update(self,x,vel):
+        keys = py.key.get_pressed()
+        if keys[py.K_a] and x > vel + 5:
+            x -= vel
+        # vel + 5 = 5.4, this is so that the border spacing on the right side is the same as the leaft side
+        if keys[py.K_d] and x < 720-self.width-5.4:
+            x += vel
+        #elif so that by holding eg. d and left arrow keys together, player won't double in speed
+        elif keys[py.K_LEFT] and x > vel + 5:
+            x -= vel
+        elif keys[py.K_RIGHT] and x < 720-self.width-5.4:
+            x += vel
+    
+    def draw(self, screen):
+        screen.blit()
 
-    keys = py.key.get_pressed()
-    if keys[py.K_a] and x > vel + 5:
-        x -= vel
-    # vel + 5 = 5.4, this is so that the border spacing on the right side is the same as the leaft side
-    if keys[py.K_d] and x < 720-width-5.4:
-        x += vel
-    #elif so that by holding eg. d and left arrow keys together, player won't double in speed
-    elif keys[py.K_LEFT] and x > vel + 5:
-        x -= vel
-    elif keys[py.K_RIGHT] and x < 720-width-5.4:
-        x += vel
-    screen.fill((255,255,255))
+def main():
+    player = spaceship()
+    running = True
+    while running:
+        for event in py.event.get():
+            if event.type == py.QUIT:
+                running = False
+        screen.fill((255,255,255))
+        py.display.flip()
 
-
-    py.draw.rect(screen,color,(x,y,width,height))
-    py.display.update()
-
-
+main()
 
 
 
